8dc19a22c468e738b33ad2ec5a4b68695b728b6c
Fuzhe <283826415@qq.com>
2020-07-20 15:33:12 +0800

    XXXX

--- process1.py
+++ process1.py
@@ -1,15 +1,17 @@
 from multiprocessing import Process
 from time import sleep
 
-def func(name,score):
+
+def func(name, score):
     for i in range(2):
         sleep(1)
-        print(name*i)
+        print(name * i)
         sleep(1)
         score += 1
         print(score)
 
-p = Process(target = func,args=("wang",99))
 
+p = Process(target=func, args=("wang", 99))
+print("gggggggg")
 p.start()
 p.join()
